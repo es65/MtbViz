246a823ee1260680676f55fae5d7af2e3e21ea6a
Evan Sims <easims@gmail.com>
2025-07-04 12:41:00 -0700

    remove old files

--- mtb/__init__.py
+++ mtb/__init__.py
@@ -7,10 +7,10 @@ A Python package for processing and analyzing mountain bike sensor data.
 from . import src
 
 # Expose key modules for easier access
-from .src import config, build_features, summary_metrics, make_dataset_sl
+from mtb.src import config, pipeline, cli
 
 __version__ = "0.1.0"
 __author__ = "Evan Sims"
 
 # Convenience imports for common functionality
-__all__ = ["config", "build_features", "summary_metrics", "make_dataset_sl", "src"]
+__all__ = ["config", "pipeline", "cli", "src"]
